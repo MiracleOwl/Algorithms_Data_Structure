1d383322ac223f03a5faabb92dc8361491e24656
MiracleOwl <valentinocvetanovski18@gmail.com>
2025-06-09 10:37:25 +0200

    Update and rename zad3_listi.java to Linkedlist_tutorial.java

--- src/Linkedlist_tutorial.java
+++ src/Linkedlist_tutorial.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class zad3_listi{
+public class Linkedlist_tutorial{
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
